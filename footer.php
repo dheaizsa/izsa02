2f092944a9a0866ae962235a854ae6f4f721fb93
dheaizsa <dheagirsang1@gmail.com>
2024-11-10 23:47:11 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Tautan Langsung</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Tautan Tambahan</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Keranjang</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="#"><i class="fas fa-phone"></i> +62 821 1103 3878</a>
+         <a href="#"><i class="fas fa-phone"></i> +62 821 1103 3878</a>
+         <a href="#"><i class="fas fa-envelope"></i>izsabell0203@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Medan, Indonesia - 1845667 </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>girsangshop</span> | all rights reserved!</div>
+
+</footer>
